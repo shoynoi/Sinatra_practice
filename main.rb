cf418967c0d75f41db31b8243b60e6d4c5cec271
吉野輝 <shoynoi.jp@gmail.com>
2019-05-15 09:07:40 +0900

    新規メモ作成機能を追加

--- main.rb
+++ main.rb
@@ -1,6 +1,52 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/' do
   erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+helpers do
+  def find_json
+    @memos = if File.exist?('memo.json')
+               File.open('memo.json') do |f|
+                 JSON.parse(f.read)
+               end
+             end
+  end
+
+  def max_id
+    @memos.nil? ? 0 : @memos.keys.map(&:to_i).max
+  end
+
+end
+
+post '/create' do
+  id = max_id + 1
+  @title = params[:title]
+  @content = params[:content]
+  created_at = Time.now
+
+  hash = { id => { title: @title, content: @content, create_at: created_at } }
+
+  if @memos
+    @memos.merge!(hash)
+    File.open('memo.json', 'w') do |f|
+      JSON.dump(@memos, f)
+    end
+  else
+    File.open('memo.json', 'w') do |f|
+      JSON.dump(hash, f)
+    end
+  end
+  redirect to("/show/#{id}")
+end
+
+get '/show/:id' do
+  @memo = @memos[params[:id]]
+  erb :show
 end
